1261897aa86b65812749ea365b4a5bb4dbbaab2c
oezcelik55 <oezcelikmete@gmail.com>
2025-12-03 13:18:14 +0300

    Sayılar yuvarlandı

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT
     date_date
-    , SUM(turnover) AS turnover
-    , SUM(purchase_cost) AS purchase_cost
+    , ROUND(SUM(turnover), 2) AS turnover
+    , ROUND(SUM(purchase_cost), 2) AS purchase_cost
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date
